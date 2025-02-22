b07ba046498fb73f67fe9fc0637d72c8c1357119
MANTHENA SRI HARSHITHA <manthenasiri23@gmail.com>
2025-02-22 22:12:22 +0530

    Update rev_num.cpp

--- rev_num.cpp
+++ rev_num.cpp
@@ -1,13 +1,17 @@
 #include<iostream>
 using namespace std;
-int main(){
+
+int main() {
     int n;
-    cin>>n;
-    int rn=0;
-    while(n<0){
-        ld=n%10;
-        rn=(rn*10)+ld;
-        n=n/10;
+    cin >> n;
+    int rn = 0;
+    
+    while(n > 0) { 
+        int ld = n % 10; 
+        rn = (rn * 10) + ld;
+        n = n / 10;
     }
-    cout<<rn;
+    
+    cout << rn;
+    return 0; 
 }
